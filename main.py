a279a2d553256a833719685d6aa90b10b33763c7
Pietro <pietroiaia98@gmail.com>
2020-01-24 23:10:57 -0400

    Agregado manejador de error cuando se ingresa el archivo a interpretar

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 from files import readFile
 
-if len(sys.argv) == 2:
-    readFile(sys.argv[1])
-elif len(sys.argv) == 1:
-    path = input("Archivo a interpretar: ")
-    readFile(path)
+try:
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+        readFile(path)
+    elif len(sys.argv) == 1:
+        path = input("Archivo a interpretar: ")
+        readFile(path)
+except:
+    print("Imposible abrir el archivo %s" % path)
