f72f7dc47fd22273d24e7bdc260286e8b31d880d
Pietro <pietroiaia98@gmail.com>
2020-01-27 21:50:43 -0400

    willy comentado y agregado mas except para diferenciar los tipos de errores

--- willy.py
+++ willy.py
@@ -2,11 +2,18 @@ import sys
 from files import readFile
 
 try:
+    # Si el usuario suministra el archivo como un argumento
     if len(sys.argv) == 2:
         path = sys.argv[1]
+        # Llama a la funcion readFile de files.py
         readFile(path)
+    # Si el usuario no suministra el archivo como un argumento, el programa ofrece un prompt para que lo haga
     elif len(sys.argv) == 1:
         path = input("Archivo a interpretar: ")
+        # Llama a la funcion readFile de files.py
         readFile(path)
+# Si el archivo no existe o no puede abrirse, el programa termina indicando el problema
+except FileNotFoundError as error:
+    print("El archivo %s no existe" % path)
 except:
     print("Imposible abrir el archivo %s" % path)
